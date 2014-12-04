aab34f822f0decd77d276e8f0bc49dcafeeee79f
Simon Walker <s.r.walker101@googlemail.com>
2014-12-04 13:42:45 +0000

    Add ensure_dir function

--- render.py
+++ render.py
@@ -4,9 +4,15 @@
 from __future__ import division, print_function, absolute_import
 import argparse
 import logging
+from os import path
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s|%(name)s|%(levelname)s|%(message)s')
 logger = logging.getLogger(__name__)
+
+def ensure_dir(p):
+    if not path.isdir(p):
+        logger.debug('Path %s does not exist, creating', p)
+        path.makedirs(p)
 
 
 def main(args):
