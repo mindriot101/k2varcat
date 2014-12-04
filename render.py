57fd62a15a6c4caad8ba1143f51339a441f82a90
Simon Walker <s.r.walker101@googlemail.com>
2014-12-04 13:43:43 +0000

    Add output-dir argument

--- render.py
+++ render.py
@@ -4,10 +4,12 @@
 from __future__ import division, print_function, absolute_import
 import argparse
 import logging
+import jinja2
 from os import path
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s|%(name)s|%(levelname)s|%(message)s')
 logger = logging.getLogger(__name__)
+
 
 def ensure_dir(p):
     if not path.isdir(p):
@@ -20,4 +22,5 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output-dir', default='build')
     main(parser.parse_args())
