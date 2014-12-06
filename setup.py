4d817e4e36ed027d3c30e21458061021a7545bd0
Simon Walker <s.r.walker101@googlemail.com>
2014-12-06 22:21:17 +0000

    Add entry point

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ setup(
     author='Simon Walker',
     author_email='s.walker.2@warwick.ac.uk',
     install_requires=['numpy', 'fitsio', 'matplotlib', 'seaborn'],
+    entry_points={'console_scripts': [
+        'k2var-render = k2var.render:main',
+    ]},
     packages=['k2var'])
