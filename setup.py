9f8666f9a8a03df84f86e02b2c915005d9ecf71c
Simon Walker <s.r.walker101@googlemail.com>
2014-12-06 22:19:27 +0000

    Make package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='k2var',
+    version='0.0.1',
+    author='Simon Walker',
+    author_email='s.walker.2@warwick.ac.uk',
+    install_requires=['numpy', 'fitsio', 'matplotlib', 'seaborn'],
+    packages=['k2var'])
