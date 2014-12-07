bc757adbfc69ecbdeb7503697371f0a2707a8e68
Simon Walker <s.r.walker101@googlemail.com>
2014-12-07 13:42:01 +0000

    Forcing rendering of object pages

--- k2var/app.py
+++ k2var/app.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from os import path
 
 BASE_DIR = path.realpath(
     path.join(
         path.dirname(__file__), '..'))
 
+DATA_DIR = path.realpath(
+    path.join(
+        path.dirname(__file__),
+        'data'))
+
 app = Flask(__name__)
 app.config['FREEZER_BASE_URL'] = 'http://localhost/'
 app.config['FREEZER_DESTINATION'] = path.join(BASE_DIR, 'build')
+
+
+def data_file_path(epicid):
+    return path.join(DATA_DIR,
+                     'ktwo{epicid}-c00_lpd-targ_X_D.fits'.format(
+                         epicid=epicid))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/objects/<string:epicid>.html')
+def render_epic_id(epicid):
+    filename = data_file_path(epicid)
+    return render_template('lightcurve.html', epicid=epicid)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
--- k2var/render.py
+++ k2var/render.py
@@ -1,8 +1,23 @@
 from k2var.app import app
 from flask_frozen import Freezer
+from os import path
+import csv
+
+freezer = Freezer(app)
+
+@freezer.register_generator
+def render_epic_id():
+    csv_filename = path.join(
+        path.dirname(__file__),
+        'K2VarCat.csv')
+    with open(csv_filename) as infile:
+        reader = csv.DictReader(infile,
+                                fieldnames=['epicid', 'type',
+                                       'range', 'period', 'amplitude'])
+        for row in reader:
+            yield {'epicid': str(row['epicid'])}
 
 def main():
-    freezer = Freezer(app)
     freezer.freeze()
 
 
