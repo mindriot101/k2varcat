e96745d111a064cdc221b161a6d8919b48ae211f
Simon Walker <s.r.walker101@googlemail.com>
2014-12-07 13:26:00 +0000

    Render to build directory

--- k2var/app.py
+++ k2var/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from os import path
+
+BASE_DIR = path.realpath(
+    path.join(
+        path.dirname(__file__), '..'))
 
 app = Flask(__name__)
+app.config['FREEZER_BASE_URL'] = 'http://localhost/'
+app.config['FREEZER_DESTINATION'] = path.join(BASE_DIR, 'build')
 
 @app.route('/')
 def index():
