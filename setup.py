5d994bce986f64179120e4742a8e71b0972e944b
Simon Walker <s.r.walker101@googlemail.com>
2014-12-07 13:17:39 +0000

    Add flask and frozen-flask to setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.0.1',
     author='Simon Walker',
     author_email='s.walker.2@warwick.ac.uk',
-    install_requires=['numpy', 'fitsio', 'matplotlib', 'seaborn'],
+    install_requires=['numpy', 'fitsio', 'matplotlib', 'seaborn',
+                      'flask', 'frozen-flask'],
     entry_points={'console_scripts': [
         'k2var-render = k2var.render:main',
     ]},
