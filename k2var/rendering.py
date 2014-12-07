72c90559954e3fdd1db0e9ce3b364a246931c81d
Simon Walker <s.r.walker101@googlemail.com>
2014-12-07 15:20:19 +0000

    Implement image plotting

--- k2var/rendering.py
+++ k2var/rendering.py
@@ -16,14 +16,6 @@ logger = logging.getLogger(__name__)
 sns.set(style='white', rc={'lines.markeredgewidth': 0.01})
 
 
-class EmptyPlot(object):
-
-    '''Class for when no periodicity exists'''
-
-    def figure(self):
-        fig, ax = plt.subplots()
-        return fig
-
 
 class Plotter(object):
 
@@ -35,6 +27,26 @@ class Plotter(object):
         logger.debug("Building figure object")
         fig, axis = plt.subplots()
         axis.errorbar(self.x, self.y, self.yerr, ls='None', marker='.')
+        return fig
+
+    def serialised(self):
+        figure = self.figure()
+        s = StringIO.StringIO()
+        figure.savefig(s, bbox_inches='tight', format='png')
+        return s.getvalue().encode('base64').strip()
+
+    def render(self):
+        return render_template('image.html', data=self.serialised())
+
+class EmptyPlot(Plotter):
+
+    '''Class for when no periodicity exists'''
+
+    def __init__(self):
+        pass
+
+    def figure(self):
+        fig, ax = plt.subplots()
         return fig
 
 
@@ -51,6 +63,39 @@ class LightcurvePlotter(object):
     def __init__(self, meta, filename):
         self.meta = meta
         self.filename = filename
+        self.data_store = DataStore(self.filename)
+
+    def raw_lightcurve(self):
+        return Plotter(
+            self.data_store['time'],
+            self.data_store['aptflux'],
+            self.data_store['aptflux_err']).render()
+
+    def detrended_lightcurve(self):
+        yerr = (self.data_store['detflux'] * (self.data_store['aptflux_err'] /
+                                              self.data_store['aptflux']))
+        return Plotter(
+            self.data_store['time'],
+            self.data_store['detflux'],
+            yerr).render()
+
+    def phase_folded(self):
+        period = self.meta['period']
+        yerr = (self.data_store['detflux'] * (self.data_store['aptflux_err'] /
+                                              self.data_store['aptflux']))
+        if period > 0:
+            phase = (self.data_store['time'] / period) % 1
+            return Plotter(
+                phase,
+                self.data_store['detflux'],
+                yerr).render()
+        else:
+            return EmptyPlot().render()
+
 
     def render(self):
-        return 'Lightcurves'
+        return render_template('plots.html',
+                               raw=self.raw_lightcurve(),
+                               detrended=self.detrended_lightcurve(),
+                               folded=self.phase_folded(),
+                               )
