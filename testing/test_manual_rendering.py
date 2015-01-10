55b267da8ab945fbb5fd8a3c97f11737e4aeb20b
Simon Walker <s.r.walker101@googlemail.com>
2015-01-10 19:50:02 +0000

    Test build_args function

--- testing/test_manual_rendering.py
+++ testing/test_manual_rendering.py
@@ -1,2 +1,12 @@
+from collections import namedtuple
+
 from k2var import cli
 
+Arguments = namedtuple('Arguments', ['root', 'output_dir'])
+
+def build_args(root, output_dir):
+    return Arguments(root, output_dir)
+
+def test_build_args():
+    args = build_args('root', 'output_dir')
+    assert args.root == 'root' and args.output_dir == 'output_dir'
