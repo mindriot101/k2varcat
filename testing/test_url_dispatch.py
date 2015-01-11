7d99ef36474ca9faa1760a0faa7bb3583f798a19
Simon Walker <s.r.walker101@googlemail.com>
2015-01-11 10:12:33 +0000

    Change url dispatch in template renderer

--- testing/test_url_dispatch.py
+++ testing/test_url_dispatch.py
@@ -6,7 +6,7 @@ from k2var.urls import UrlFor
 def url_root():
     return ''
 
+
 def test_static_path(url_root):
     u = UrlFor(url_root)
     assert u('static', filename='test.css') == '/static/test.css'
-
