2c6e7cefc350b4950754b390c9d3eb5e30de74e2
Simon Walker <s.r.walker101@googlemail.com>
2015-01-13 22:05:05 +0000

    Add test for emptyplot and render_template

---
+++ testing/test_plotter.py
@@ -0,0 +1,23 @@
+from matplotlib.pyplot import Figure
+from unittest import mock
+import pytest
+
+from k2var import rendering
+from k2var.rendering import RendersTemplates
+
+
+@pytest.fixture
+def url_root():
+    return '/'
+
+
+def test_render_template(url_root):
+    template_stub = 'template_stub'
+    with mock.patch.object(RendersTemplates, '__getitem__') as getitem:
+        rendering.render_template(url_root, template_stub)
+        getitem.assert_called_once_with(template_stub)
+
+
+def test_empty_plotter(url_root):
+    e = rendering.EmptyPlot(url_root)
+    assert isinstance(e.figure(), Figure)
