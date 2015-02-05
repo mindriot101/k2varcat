ea68d8fc15c162676cf4eff6c3896c4ba5a69eef
Simon Walker <s.r.walker101@googlemail.com>
2015-02-05 09:32:18 +0000

    Full integration test of render_page

---
+++ testing/test_tasks.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+import itertools
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from k2var.data_store import Database
+from k2var import cli
+from k2var import tasks
+
+
+@pytest.fixture
+def db():
+    return Database()
+
+
+@pytest.fixture
+def valid_epicid(db):
+    return list(itertools.islice(db.valid_epic_ids(), 1))[0]
+
+
+@pytest.fixture
+def ensure_output_dir(tmpdir):
+    output_dir = str(tmpdir)
+    args = mock.Mock(root='/', output_dir=output_dir)
+    app = cli.K2Var(args)
+    app.ensure_output_dir()
+    return output_dir
+
+
+def test_render_page(valid_epicid, ensure_output_dir):
+    output_dir = ensure_output_dir
+    root_url = '/'
+    tasks.render_page(output_dir, root_url, valid_epicid)
+
+    assert os.path.isfile(
+        os.path.join(output_dir, 'objects', '{}.html'.format(valid_epicid)
+                     ))
