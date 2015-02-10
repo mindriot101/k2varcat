2065688af497e2114a9352a3d7cb3be8b676749f
Simon Walker <s.r.walker101@googlemail.com>
2015-02-10 16:23:44 +0000

    Add test for ensuring valid campaigns

--- testing/test_database.py
+++ testing/test_database.py
@@ -17,3 +17,19 @@ def test_valid_epic_ids(path):
             db = data_store.Database(None)
             assert [items[0] for items in
                     list(db.valid_epic_ids(campaigns=[0, ]))] == [epicid, ]
+
+
+@mock.patch('k2var.data_store.Database.load_data')
+def test_valid_epic_ids_with_fixtures(load_data,
+                                      epicid_campaign_0,
+                                      epicid_campaign_1,
+                                      filename_campaign_0,
+                                      filename_campaign_1):
+    load_data.return_value = {
+        epicid_campaign_0: None,
+        epicid_campaign_1: None
+    }
+    db = data_store.Database(None)
+    valid_ids = list(db.valid_epic_ids(campaigns=[0, 1]))
+    assert sorted(valid_ids) == sorted([(epicid_campaign_0, 0),
+                                        (epicid_campaign_1, 1)])
