e3c84dd487b3bb1934132d7e78bd9ccdc86f3862
Simon Walker <s.r.walker101@googlemail.com>
2015-02-11 21:20:42 +0000

    Add request class to help tests

--- testing/test_test_all.py
+++ testing/test_test_all.py
@@ -2,9 +2,22 @@ import urllib.request
 
 from k2var import test_all
 
+class Request(object):
+    @classmethod
+    def get(cls, port, url='http://localhost'):
+        return cls(urllib.request.urlopen(':'.join([url, str(port)])))
+
+    def __init__(self, response):
+        self.response = response
+
+    @property
+    def status_code(self):
+        return self.response.status
+
+
 def test_run_server():
     port = 10105
     s = test_all.run_server(port)
-    s.join(1)
-    response = urllib.request.urlopen('http://127.0.0.1:{}'.format(port))
-    assert response.status == 200
+    s.join(0.1)
+    response = Request.get(port)
+    assert response.status_code == 200
