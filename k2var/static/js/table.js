02f13c4691b1c814b07f52f92d9b9a7d853d840a
Simon Walker <s.r.walker101@googlemail.com>
2015-05-07 21:25:39 +0100

    Switch to basic data tables

---
+++ k2var/static/js/table.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('#summary').DataTable({
+        processing: true,
+        pageLength: 25,
+        ajax: {
+            url: "https://spreadsheets.google.com/feeds/list/11Y0WIAQwOVJi-2mP9oVEzHg1EActd6sNxaYfwZT1SB4/od6/public/values?alt=json",
+            dataSrc: "feed.entry",
+        },
+        columns: [
+            { "data": "gsx$epicid.$t" },
+            { "data": "gsx$type.$t" },
+            { "data": "gsx$range.$t" },
+            { "data": "gsx$period.$t" },
+            { "data": "gsx$amplitude.$t" },
+            { "data": "gsx$proposal.$t" },
+        ],
+    });
+});
