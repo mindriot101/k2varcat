3368d2f2f1dd7c1859497eedd1d2b00207d137c7
Simon Walker <s.r.walker101@googlemail.com>
2015-05-23 13:04:52 +0100

    Remove unrequired root argument

--- k2var/only_pngs.py
+++ k2var/only_pngs.py
@@ -30,9 +30,6 @@ def only_pngs(args):
 def main():
     default_output = os.path.join(BASE_DIR, 'build')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--root',
-                        default='',
-                        help='Application root. For phsnag: /phsnag/')
     parser.add_argument('-o', '--output-dir', default=default_output, required=False)
     parser.add_argument('-d', '--metadata-csv', required=True)
     only_pngs(parser.parse_args())
